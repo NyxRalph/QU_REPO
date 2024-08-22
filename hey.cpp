ddf860dd577b20c9e3e909a6a8abbc3e00385461
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-08-22 23:24:44 +0000

    ...

---
+++ hey.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+
+int main (){
+double driver = 'Ralph' ;
+cout << 'Driver is ;' << driver <<endl;
+double destination ;
+cout << "what's your destination" ;
+cin >> destination;
+}
