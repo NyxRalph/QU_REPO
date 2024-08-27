3f27a2cf7719bac566630f6857c67afeed5492dd
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-08-27 21:42:33 +0000

    ...

--- QU_1.js
+++ QU_1.js
@@ -3,9 +3,3 @@
 const integer = 34;
 const decimal = integer.toString(2)
 console.log(decimal)
-const integer = 34;
-const decimal = integer.toString(2)
-console.log(decimal)
-const integer = 34;
-const decimal = integer.toString(2)
-console.log(decimal)
